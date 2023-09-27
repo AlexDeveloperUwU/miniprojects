832f16ccc65d88851191002d6a0b65f540909c91
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2023-09-27 14:17:52 +0200

    wen

---
+++ codewars/web/clans.js
@@ -0,0 +1,115 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = 5421;
+
+let DAW_totalHonor, DAW_totalScore;
+let DAM_totalHonor, DAM_totalScore;
+let diferenciaHonor;
+let diferenciaScore;
+let txtHonor;
+let txtScore;
+
+function actualizarDatos() {
+  axios
+    .get("https://www.codewars.com/api/v1/clans/1DAW_O_TEIS/members")
+    .then((response) => {
+      if (response.status === 200) {
+        const data = response.data;
+        const userData = data.data;
+        function sumarHonorYScore(usuarios) {
+          let totalHonor = 0;
+          let totalScore = 0;
+
+          for (const usuario of usuarios) {
+            totalHonor += usuario.honor;
+            totalScore += usuario.score;
+          }
+
+          DAW_totalHonor = totalHonor;
+          DAW_totalScore = totalScore;
+        }
+
+        sumarHonorYScore(userData);
+      }
+    });
+
+  axios
+    .get("https://www.codewars.com/api/v1/clans/2teis/members")
+    .then((response) => {
+      if (response.status === 200) {
+        const data = response.data;
+        const userData = data.data;
+        function sumarHonorYScore(usuarios) {
+          let totalHonor = 0;
+          let totalScore = 0;
+
+          for (const usuario of usuarios) {
+            totalHonor += usuario.honor;
+            totalScore += usuario.score;
+          }
+
+          DAM_totalHonor = totalHonor;
+          DAM_totalScore = totalScore;
+        }
+
+        sumarHonorYScore(userData);
+      }
+    });
+
+  const sleep = (waitTimeInMs) =>
+    new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
+  sleep(1000).then(() => {
+    if (DAW_totalHonor > DAM_totalHonor) {
+      diferenciaHonor = DAW_totalHonor - DAM_totalHonor;
+      txtHonor = `Gana DAW con una diferencia de ${diferenciaHonor} de honor`;
+    } else if (DAW_totalHonor < DAM_totalHonor) {
+      diferenciaHonor = DAM_totalHonor - DAW_totalHonor;
+      txtHonor = `Gana DAM con una diferencia de ${diferenciaHonor} de honor`;
+    } else {
+      txtHonor = "Ambos tienen el mismo honor";
+    }
+
+    if (DAW_totalScore > DAM_totalScore) {
+      diferenciaScore = DAW_totalScore - DAM_totalScore;
+      txtScore = `Gana DAW con una diferencia de ${diferenciaScore} de puntuación`;
+    } else if (DAW_totalScore < DAM_totalScore) {
+      diferenciaScore = DAM_totalScore - DAW_totalScore;
+      txtScore = `Gana DAM con una diferencia de ${diferenciaScore} de puntuación`;
+    } else {
+      txtScore = "Ambos tienen la misma puntuación";
+    }
+  });
+}
+
+actualizarDatos();
+
+setInterval(actualizarDatos, 60000);
+
+app.set("port", process.env.PORT || 80);
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    DAW_totalHonor,
+    DAW_totalScore,
+    DAM_totalHonor,
+    DAM_totalScore,
+    txtHonor,
+    txtScore,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
