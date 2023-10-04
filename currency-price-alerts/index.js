f270142afa0535966277d1398c13ad2308060485
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2023-10-04 09:40:15 +0200

    Update index.js

--- currency-price-alerts/index.js
+++ currency-price-alerts/index.js
@@ -2,26 +2,16 @@
 
 const axios = require('axios');
 
-// URL de la API de tasas de conversión
 const API_URL = 'https://v6.exchangerate-api.com/v6/TOKEN/latest/USD';
-
-// URL del webhook de Discord
 const WEBHOOK_URL = 'DISCORD WEBHOOK URL';
+const INTERVALO_VERIFICACION = 175000;
 
-// Tiempo de intervalo para verificar las tasas de conversión (en milisegundos)
-const INTERVALO_VERIFICACION = 175000; // 1 minuto
-
-// Función para enviar un mensaje al webhook de Discord
 async function enviarMensajeDiscord(mensaje) {
   await axios.post(WEBHOOK_URL, {
     content: mensaje,
   });
 }
 
-// Mensaje de inicio en la consola
-console.log('El script de verificación de tasas de conversión se ha iniciado.');
-
-// Mensaje de inicio en Discord
 enviarMensajeDiscord('El script de verificación de tasas de conversión se ha iniciado.');
 
 async function verificarTasasDeConversion() {
@@ -30,16 +20,11 @@ async function verificarTasasDeConversion() {
     if (response.status === 200) {
       const data = response.data;
       const tasaUSDToEUR = data.conversion_rates.EUR;
-      // Establece aquí la tasa de conversión que consideras como "la mejor"
       const mejorTasa = 0.94;
 
       if (tasaUSDToEUR >= mejorTasa) {
-        // Mensaje a enviar al webhook
         const mensaje = `¡La tasa de conversión USD a EUR es favorable (${tasaUSDToEUR})!`;
-
-        // Envía el mensaje al webhook de Discord
         await enviarMensajeDiscord(mensaje);
-
         console.log('Mensaje enviado a Discord:', mensaje);
       }
     } else {
