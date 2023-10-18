c13ef464d402277e062313c030ed21e37d3edfaf
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2023-10-18 11:23:10 +0200

    updates

--- msgsender/v2/app.js
+++ msgsender/v2/app.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-io.on('connection', (socket) => {
-  const clientIp = socket.handshake.address.replace(/^::ffff:/, ''); // Eliminar '::ffff:' de la dirección IP
+io.on("connection", (socket) => {
+  const clientIp = socket.handshake.address.replace(/^::ffff:/, ""); // Eliminar '::ffff:' de la dirección IP
   console.log(`Usuario conectado desde la dirección IP: ${clientIp}`);
 
-  socket.on('message', (message) => {
-    io.emit('message', `${clientIp} => ${message}`);
-    if (message.toLowerCase().includes('hola')) {
-      const botResponse = '¡Hola! Soy un bot.';
+  socket.on("message", (message) => {
+    io.emit("message", `${clientIp} => ${message}`);
+    if (message.toLowerCase().includes("hola")) {
+      const botResponse = "¡Hola! Soy un bot.";
       setTimeout(() => {
-        io.emit('message', `Bot: ${botResponse}`);
+        io.emit("message", `Bot: ${botResponse}`);
       }, 1000);
     }
   });
 });
 
 server.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+  console.log("Servidor escuchando en el puerto 3000");
 });
--- msgsender/v2/public/client.js
+++ msgsender/v2/public/client.js
@@ -1,37 +1,39 @@
 const socket = io();
-const messageInput = document.getElementById('message');
-const messagesContainer = document.getElementById('messages');
+const messageInput = document.getElementById("message");
+const messagesContainer = document.getElementById("messages");
 
-messageInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+messageInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     sendMessage();
   }
 });
 
-document.getElementById('sendButton').addEventListener('click', () => {
+document.getElementById("sendButton").addEventListener("click", () => {
   sendMessage();
 });
 
 function sendMessage() {
   const message = messageInput.value;
-  if (message.trim() !== '') {
-    socket.emit('message', message);
-    messageInput.value = '';
+  if (message.trim() !== "") {
+    socket.emit("message", message);
+    messageInput.value = "";
   }
 }
 
-socket.on('message', (message) => {
-  const li = document.createElement('li');
+socket.on("message", (message) => {
+  const li = document.createElement("li");
 
-  const parts = message.split(' => ');
+  const parts = message.split(" => ");
   if (parts.length === 2) {
-    li.textContent = `${parts[0]}: ${parts[1]}`;
+    const username = parts[0];
+    const messageText = parts[1];
+
+    const messageHTML = `<b>${username}:</b> ${messageText}`;
+    li.innerHTML = messageHTML;
   } else {
     li.textContent = message;
   }
 
   messagesContainer.appendChild(li);
-
-  // Scroll hacia abajo para mostrar el último mensaje
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
 });
